9ce8fbfaa7ec80cd6047d2e1ef9e0a579838b98f
Roman Bolshakov <rombolshak@gmail.com>
2011-11-13 21:23:58 +0400

    Comments should be before prototype

--- Feistel network.h
+++ Feistel network.h
@@ -15,33 +15,36 @@ class FNetwork
     
     FNetwork(int roundsNo = 3) : roundsNo(roundsNo) {}
     
-    string encrypt (string msg, string key);
-	/* Дополнить сообщение (например \0 -ми)до длины кратной 16 символам (128 бит)
+    /* Дополнить сообщение (например \0 -ми)до длины кратной 16 символам (128 бит)
 	 * Для каждого куска в 64 бита применить roundsNo раз итерацию шифрования с
 	 * заданной F, записывая резултата в строку.
 	 * Вернуть эту строку
 	 * */
-    string decrypt (string msg, string key);
+    string encrypt (string msg, string key);
+	
 	/* Для каждого куска по 128 бит применить roundsNo раз итерацию шифрования с
 	 * заданной F, записывая резултата в строку (Ключи подаются в обратном порядке).
 	 * Вернуть эту строку
 	 * */
-    
-    void fillRandArray(unsigned long seed);
+    string decrypt (string msg, string key);
+	
 	/* Заполнить массив R сл. числами с порождающим
 	 * элементом seed
 	 * */
+    void fillRandArray(unsigned long seed);
+	
     inline int countBlocks(string msg)
     {
 	int len = msg.length();
 	return len%4 == 0 ? len/4 : len/4 + 1;
     }
     
+    /* Сгенерировать массив keys из (countBlocks*roundsNo) ключей. 
+	 * i-й ключ пердназнаечн для операций i/roundsNo -й строкой
+	 * на i%roundsNo -й итерации сети Фейстеля
+	 * */
     void generateRoundKeys();
- 	/* Сгенерировать массив keys из (countBlocks*roundsNo) ключей. 
-	* i-й ключ пердназнаечн для операций i/roundsNo -й строкой
-	* на i%roundsNo -й итерации сети Фейстеля
-	* */
+ 	
     
     string strxor (string a, string b)
     {
